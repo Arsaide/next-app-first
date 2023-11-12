73d5cd846b01999d5fb8c8db0ff0848f04542942
Kirill <93765186+Arsaide@users.noreply.github.com>
2023-11-13 01:15:10 +0200

    Initial commit

---
+++ app/blog/page.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+import { Posts } from "@/components/Posts";
+import { PostSearch } from "@/components/PostSearch";
+
+export const metadata: Metadata = {
+  title: "Blog | Next App",
+};
+
+export default function Blog() {
+  return (
+    <>
+      <h1>Blog page</h1>
+      <PostSearch />
+      <Posts />
+    </>
+  );
+}
